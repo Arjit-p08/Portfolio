3371f064e722b7de4ade6d24af9b7354f320f6fe
[Arjit Pandey] <[arjitpanday1@gmail.com]>
2024-04-16 22:08:53 +0530

    initc

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Kanishk Sharma",
+  title: "Arjit Pandey",
   description: "PortFolio App",
 };
 
